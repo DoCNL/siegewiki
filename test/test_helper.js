495d1c63e3f99630d89e148fcc2ee02d3b5edbb2
Sjoerd van der Bok <sabok@avans.nl>
2018-12-12 08:16:59 +0100

    added register route test

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose')
-const mongodb = require('../config/mongodb_connector');
+mongoose.Promise = global.Promise
 
-before(() => {
+before((done) => {
     mongoose.disconnect();
-    mongodb.createTestConnection();
+    mongoose.connect('mongodb://localhost/siegewiki_test', { useNewUrlParser: true })
+    var connection = mongoose.connection
+        .once('open', () => {
+            console.log('Connected to Mongo on localhost to test')
+            done();
+        })
+        .on('error', (error) => {
+            console.warn('Warning', error.toString());
+        });
 });
 
-// beforeEach((done) => {
-//         mongoose.connection.collections.users.drop(() => {
-//             mongoose.connection.collections.operators.drop(() => {
-//                 mongoose.connection.collections.seasons.drop(() => {
-//                     mongoose.connection.collections.siegemaps.drop(() => {
-//                     done();
-//                     })
-//                 })
-//             })
-//         })
-// })
--- test/user_test.js
+++ test/user_test.js
@@ -1,28 +1,57 @@
 const assert = require('assert')
-const request = require('supertest')
 const mongoose = require('mongoose')
 const User = require('../src/models/user')
-const app = require('../server')
+var app = require('../server'),
+  chai = require('chai'),
+  request = require('supertest');
+var expect = chai.expect;
+
 // var chaiHttp = require('chai-http');
 // chai.use(chaiHttp)
 // chai.should()
 
-describe('The usercontroller can', () => {
-    it('can create a new user', (done) => {
+describe('The usercontroller can',  function(){
+    this.timeout(0);
+
+    var user = {
+        name: 'testnameChai',
+        password: 'testpasswordChai'
+    };
+    it('should register a user', function(done) { 
+    request(app)
+        .post('/api/user/register')
+        .send(user)
+        .end(function(err, res) { 
+            expect(res.statusCode).to.equal(200);  
+            done(); 
+        }); 
+    });
+
+    it('should fetch all seasons', function(done) {
+        this.timeout(15000);
         request(app)
-            .post('/api/user/register')
-            .send({
-                name: 'TestUser',
-                password: 'TestPassword'
+            .get('/api/seasons')
+            .end(function(err, res) {
+                if(err) console.log(err);
+                expect(res.statusCode).to.equal(200); 
+                expect(res.body).to.be.an('array'); 
+                expect(res.body).to.be.empty; 
+                done(); 
             })
-            .end(() => {
-            User.findOne({
-                name: 'TestUser',
-                password: 'TestPassword'
-            }).then(user => {
-                assert(user !== 'null')
-                done();
-            })
-        })
+    })
+});
+
+beforeEach( function(done) {
+    this.timeout(0);
+    const { users, operators, seasons, siegemaps } = mongoose.connection.collections;
+
+    users.drop(() => {
+        siegemaps.drop(() => {
+            operators.drop(() => {
+                seasons.drop(() => {
+                    done();
+                });
+            });
+        });
     });
 });
