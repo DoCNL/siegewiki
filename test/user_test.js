c8d44fe7f545d4ae2fd90e83141fc43a125a2814
Sjoerd van der Bok <sabok@avans.nl>
2018-12-14 09:13:18 +0100

    final test fix

--- test/user_test.js
+++ test/user_test.js
@@ -9,69 +9,32 @@ chai.use(chaiHttp);
 
 describe('The usercontroller can', function () {
     this.timeout(0);
-
     var user = {
         name: 'testnameChai',
         password: 'testpasswordChai',
         newPassword: 'nieuwepaslmao'
     };
 
-    xit('shsould register a user', function (done) {
-        request(app)
-            .post('/api/user/register')
-            .send(user)
-            .end(function (err, res) {
-                //console.log(res.body)
-                expect(res.statusCode).to.equal(200);
-                done();
-            });
-    });
-
-    xit('should log a user in', function (done) {
-
-        request(app)
-            .post('/api/user/register')
-            .send(user)
-            .end(function (err, res) {
-                //console.log(res.body)
-                expect(res.statusCode).to.equal(200);
-            })
-        request(app)
-            .post('/api/user/login')
-            .send(user)
-            .end(function (err, res) {
-                //if (err) console.log(err);
-                //if (res) console.log(res);
-                //expect(res.statusCode).to.equal(200);  
-                //expect(res.body.auth).to.equal(true);
-
-                done();
-            });
-    })
-
-    xit('should log a user in', function (done) {
+    it('should register a user', function (done) {
+        User.collection.drop(() => {
         var token = 'Bearer ';
-        chai.request(app)
+        request(app)
             .post('/api/user/register')
             .send(user)
-            .then(function (err, res) {
-                //if (err) console.log(err)
-                token = 'Bearer ' + res.body.token
-                console.log(token)
+            .end(function (err, res) {
+                console.log(res.body)
+                expect(res.body.auth).to.equal(true);
                 expect(res.statusCode).to.equal(200);
+                token = 'Bearer ' + res.body.token;
                 chai.request(app)
-                .put('/api/user/')
-                .set({ 'Authorization': token })
-                .send({user})
-                .end(function (err, res) { 
-                    console.log(token);
-                    expect(res.statusCode).to.equal(200);
-                    //expect(res.body.auth).to.equal(true);
-    
-                    done();
-                });
-            })
-            .then((done) => { 
-            })
-    })
+                    .put('/api/user/')
+                    .set({ 'Authorization': token })
+                    .send(user)
+                    .end(function (err, res) {
+                        expect(res.statusCode).to.equal(200);
+                        done();
+                    });
+            });
+        });
+    });
 });
